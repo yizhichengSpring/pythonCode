61624c68f2eb17dcb9067aaa9c51de08f8d3e1f0
yizhicheng <yizc@emulian.com>
2018-07-30 14:46:24 +0800

    添加公司要求和发布时间

--- python_one/boss/findWork.py
+++ python_one/boss/findWork.py
@@ -4,13 +4,22 @@ import csv
 import datetime
 
 class Work(object):
-
-    work = ''
+    #地区
+    area = ''
+    #薪资
     salary = ''
-    name = ''
-    detailWork = ''
-    jobDetail = ''
-
+    #公司名称
+    companyName = ''
+    #公司信息
+    companyDetail = ''
+    #链接
+    companyHref = ''
+    #公司要求
+    companyRequirement = ''
+    #本科数量
+    undergraduate = 0
+    #发布时间
+    releaseTime = ''
 
     def fakerBrowser(self):
         # 模拟浏览器请求
@@ -42,10 +51,15 @@ class Work(object):
         jobDetail = html.xpath('//div[@class="company-text"]//p')
         # 公司链接
         href = html.xpath('//div[@class="info-primary"]//h3[@class="name"]/a/@href')
-        return work,salary,name,jobDetail,href
+        # 要求
+        requirement = html.xpath('//div[@class="info-primary"]//p')
+        #发布时间
+        releaseTime=html.xpath('//div[@class="info-publis"]//p//text()')
+        return work,salary,name,jobDetail,href,requirement,releaseTime
 
 
     def getData(self,pageNum):
+        undergraduateNum = 0
         workList = list()
         findWork = Work()
         # 获得requestheaders
@@ -55,17 +69,20 @@ class Work(object):
         num = 30
         r = requests.get('https://www.zhipin.com/c101010100-p100101/?page='+str(pageNum)+'&ka=page-'+str(pageNum)+'', headers=headers)
         prefix = 'https://www.zhipin.com'
-        gangwei, money, name, content, href = findWork.parse_one_page(r.text)
+        area, salary, companyName, companyDetails, companyHrefs,companyRequirements,releaseTimes = findWork.parse_one_page(r.text)
         for n in range(num):
-            jobdetail = prefix + href[n]
-            detailWork = content[n].xpath('string(.)').strip()
-            print(gangwei[n] + "\t", money[n] + "\t", name[n] + "\t", jobdetail + "\t", detailWork + "\t")
+            companyHref = prefix + companyHrefs[n]
+            companyDetail = companyDetails[n].xpath('string(.)').strip()
+            companyRequirement = companyRequirements[n].xpath('string(.)').strip()
+            print(area[n] + "\t", salary[n] + "\t", companyName[n] + "\t", companyHref + "\t", companyDetail + "\t",companyRequirement+"\t",releaseTimes[n]+"\t")
             workEntity = Work()
-            workEntity.work = gangwei[n]
-            workEntity.salary = money[n]
-            workEntity.name = name[n]
-            workEntity.detailWork = detailWork
-            workEntity.jobDetail = jobdetail
+            workEntity.area = area[n]
+            workEntity.salary = salary[n]
+            workEntity.companyName = companyName[n]
+            workEntity.companyDetail = companyDetail
+            workEntity.companyHref = companyHref
+            workEntity.companyRequirement = companyRequirement
+            workEntity.releaseTime = releaseTimes[n]
             workList.append(workEntity)
 
         findWork.setCsv(workList,pageNum)
@@ -76,10 +93,15 @@ class Work(object):
             dataName = "data"+str(pageNum);
             with open(dataName+".csv", 'w') as csvfile:
                 writer = csv.writer(csvfile)
-                writer.writerow(['岗位', '薪水', '公司名称', '详细信息', '公司链接'])
+                writer.writerow(['岗位', '薪水', '公司名称', '详细信息', '公司链接','公司要求','发布时间'])
                 for work in list:
-                    print(work.name)
-                    writer.writerow([work.work, work.salary, work.name, work.detailWork, work.jobDetail])
+                    writer.writerow([work.area, work.salary, work.companyName, work.companyDetail, work.companyHref,work.companyRequirement,work.releaseTime])
+
+
+    def findUndergraduate(self,companyRequirement):
+        if companyRequirement.find("本科")>=0:
+            return True
+
 
 num = 1;
 while (num):

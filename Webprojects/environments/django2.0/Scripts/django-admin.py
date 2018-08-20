00e44de5f79db7967ec2207cd4b01d8f0fd136f7
yizhichengSpring <13141016707@163.com>
2018-08-20 12:19:00 +0800

    删除django部分

--- Webprojects/environments/django2.0/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\github-code\pythoncode\webprojects\environments\django2.0\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- Webprojects/venv/Scripts/django-admin.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-#!c:\github-code\pythoncode\webprojects\venv\scripts\python.exe
-=======
-#!d:\github-code\pythoncode\webprojects\venv\scripts\python.exe
->>>>>>> b6c47195d68bf438d50153265b824fab6e10db61
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
